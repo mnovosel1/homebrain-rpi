44f8d30d288d5b0287ff4f7409b5405cdaf24d5b
Marijo <marijo@bubulescu.org>
2018-09-16 15:39:18 +0200

    Amp fix, no /usr/bin

--- classes/class.Amp.php
+++ classes/class.Amp.php
@@ -13,18 +13,18 @@ class Amp {
     
     public static function on() {        
         // SYSTEM_POWER
-        exec("sudo /usr/bin/nrf 1 irnec:5EA1B847 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA1B847 &");
     }
     
     public static function off() {
         // STANDBY
-        exec("sudo /usr/bin/nrf 1 irnec:5EA17887 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA17887 &");
     }
 
     public static function volUp($count = 1) {
         // VOLUME_UP
         for ($i = 0; $i < $count; $i++)
-            exec("sudo /usr/bin/nrf 1 irnec:5EA158A7 &");
+            exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA158A7 &");
     }
 
     public static function volUp1() {
@@ -37,13 +37,13 @@ class Amp {
 
     public static function mute() {
         // MUTE
-        exec("sudo /usr/bin/nrf 1 irnec:5EA138C7 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA138C7 &");
     }
 
     public static function volDown($count = 1) {
         // VOLUME_DOWN
         for ($i = 0; $i < $count; $i++)
-            exec("sudo /usr/bin/nrf 1 irnec:5EA1D827 &");
+            exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA1D827 &");
     }
 
     public static function volDown1() {
@@ -56,32 +56,32 @@ class Amp {
 
     public static function mpd() {
         // MD_CDR_INPUT
-        exec("sudo /usr/bin/nrf 1 irnec:5EA1936C &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA1936C &");
     }
 
     public static function kodi() {
         // D-TV_CBL_INPUT
-        exec("sudo /usr/bin/nrf 1 irnec:5EA12AD5 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA12AD5 &");
     }
 
     public static function aux() {
         // V-AUX_INPUT
-        exec("sudo /usr/bin/nrf 1 irnec:5EA1AA55 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA1AA55 &");
     }
 
     public static function movie() {
         // MOVIE_THEATRE_1
-        exec("sudo /usr/bin/nrf 1 irnec:5EA1718E &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA1718E &");
     }
         
     public static function dolby() {
         // DOLBY_DIGITAL_OR_DTS
-        exec("sudo /usr/bin/nrf 1 irnec:5EA109F6 &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA109F6 &");
     }
             
     public static function music() {
         // ROCK_CONCERT
-        exec("sudo /usr/bin/nrf 1 irnec:5EA151AE &");
+        exec("sudo ". DIR ."/bin/nrf 1 irnec:5EA151AE &");
     }
 }
 

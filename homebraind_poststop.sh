84f296318ac6a806ca42ebaee8f7a1e51eb58087
Marijo <marijo@bubulescu.org>
2018-10-02 15:31:21 +0200

    fixed hbrain dbbackup path on poststart

--- homebraind_poststop.sh
+++ homebraind_poststop.sh
@@ -3,7 +3,7 @@
 date=`date +"%d-%m-%Y"`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-hbrain dbbackup
+$DIR/homebrain hbrain dbbackup
 
 echo "Backup /home/hbrain/.node-red to node-red_$date.tgz...."
 tar -zcf /srv/PiStorage/backups/node-red_$date.tgz /home/hbrain/.node-red/

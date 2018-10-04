7c5530b0b4a69eacae2278af5e711a209a87d8e4
Marijo <marijo@bubulescu.org>
2018-10-04 19:34:20 +0200

    index.html changed to index.php

---
+++ www/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HomeBrain</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    body {
+      background-color: #1abc9c;
+    }
+  .bg-1 { 
+      color: #fefefe;
+  }
+  .btn {
+    height: 100px;
+  }
+  </style>
+</head>
+<body>
+
+  <div class="bg-1">
+    <div class="container text-center">
+      <br>
+      <a href="#"><h1>HomeBrain</h1></a><br>
+      <a href="#"><h1>Financije</h1></a><br>
+    </div>
+  </div>
+
+</body>
+</html>

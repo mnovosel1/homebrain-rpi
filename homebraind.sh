27e1158e2b8b73c04a103368eeca15a5757ddfa0
Marijo <marijo@bubulescu.org>
2018-10-22 13:29:01 +0200

    code formatting

--- homebraind.sh
+++ homebraind.sh
@@ -14,9 +14,8 @@ if [ "$lasttime" != "$nowtime" ]; then
   lasttime=$nowtime
   echo $nowtime
 
-  # every midnight
-  case $nowtime in
-    (00:00)
+  #### every midnight
+  case $nowtime in (00:00)
 	$DIR/homebrain hbrain uploadData
 	$DIR/homebrain hbrain dbbackup
 
@@ -30,59 +29,53 @@ if [ "$lasttime" != "$nowtime" ]; then
 	;;
   esac
 
-  # every day at 2:22
-  case $nowtime in
-    (2:22)
+  #### every day at 2:22
+  case $nowtime in (2:22)
 	$DIR/homebrain hbrain allOff
 	sudo /sbin/shutdown -F -r now
 	;;
   esac
 
-  # every hour
-  case $nowtime in
-    (*:00)
+  #### every hour
+  case $nowtime in (*:00)
 	$DIR/homebrain hbrain todo
 	$DIR/homebrain hbrain wifi
 	$DIR/homebrain lan checknetwork
 	;;
   esac
 
-  # every 30 minutes
-  case $nowtime in
-    (*:[03]0)
+  #### every 30 minutes
+  case $nowtime in (*:[03]0)
 	echo 'every 30 minutes'
 	;;
   esac
 
-  case $nowtime in
-    (*:*[0])
+  case $nowtime in (*:*[0])
 	echo 'every 10 minutes'
 	;;
   esac
 
-  # every 5 minutes
-  case $nowtime in
-    (*:*[05])
+  #### every 5 minutes
+  case $nowtime in (*:*[05])
 	$DIR/homebrain hbrain wakecheck
 	;;
   esac
 
-  # every 2 minutes
-  case $nowtime in
-    (*:*[02468])
+  #### every 2 minutes
+  case $nowtime in (*:*[02468])
 	echo 'every 2 minutes'
 	;;
   esac
 
-  # every minute
-  case $nowtime in
-    (*)
+  #### every minute
+  case $nowtime in (*)
 	$DIR/homebrain medvedi check
 	$DIR/homebrain hbrain alarm
 	;;
   esac
 
   sleep 30
+
 fi
 
 done

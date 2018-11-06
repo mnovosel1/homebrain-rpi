22b676283506d1d28a77b4b63d7f484789012c61
Marijo <marijo@bubulescu.org>
2018-11-06 13:41:30 +0100

    isLoud method implemented

--- classes/class.Sound.php
+++ classes/class.Sound.php
@@ -11,7 +11,16 @@ class Sound {
     }
 
     public static function isOn() {
-    	return Sound::isLoud();
+	if ( Sound::isLoud() ) {
+		debug_log(__METHOD__.":".__LINE__, "Sound is loud..");
+			SQLITE::update("states", "active", 1, "`name`='Sound'");
+			return "true";
+	}
+
+        else {
+               SQLITE::update("states", "active", 0, "`name`='Sound'");
+               return "false";
+        }
     }
 
     public static function isLoud() {

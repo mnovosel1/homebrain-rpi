8d11470f55e6c3cf88c0e69b78f16d24696187be
Marijo <marijo@bubulescu.org>
2018-11-06 15:45:28 +0100

    Sound average last 10 minutes

--- classes/class.Sound.php
+++ classes/class.Sound.php
@@ -24,7 +24,7 @@ class Sound {
     }
 
     public static function isLoud() {
-	$res = SQLITE::query("SELECT AVG(sound) FROM datalog WHERE DATETIME(timestamp) >= DATETIME('now', '-30 minutes')");
+	$res = SQLITE::query("SELECT AVG(sound) FROM datalog WHERE DATETIME(timestamp) >= DATETIME('now', '-10 minutes')");
 	return $res[0]["AVG(sound)"] > Configs::get("SOUND", "MAX");
     }
 }

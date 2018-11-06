261d408561decfe16684c359c7e8870be9db0ccf
Marijo <marijo@bubulescu.org>
2018-11-06 11:52:50 +0100

    class Sound added

---
+++ classes/class.Sound.php
@@ -0,0 +1,21 @@
+<?php
+
+class Sound {
+
+    public static function h() {
+        return MyAPI::help(self::class);
+    }
+
+    public static function help() {
+        return MyAPI::help(self::class);
+    }
+
+    public static function isOn() {
+    	return Sound::isLoud();
+    }
+
+    public static function isLoud() }
+	return false;
+    }
+
+?>

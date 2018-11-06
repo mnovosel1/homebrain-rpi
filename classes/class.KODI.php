71adcab43912cabc6ed2c453db745cd0508a0bd0
Marijo <marijo@bubulescu.org>
2018-11-06 14:31:11 +0100

    KODI on method fixed

--- classes/class.KODI.php
+++ classes/class.KODI.php
@@ -25,40 +25,32 @@ class KODI {
 
 	public static function isOn() {
 
-			if ( LAN::SSH("KODI", 
+			if ( LAN::SSH("KODI",
 					"if pgrep -x 'kodi' >/dev/null; then echo 'on'; else echo 'off'; fi") == "on" ) {
 				SQLITE::update("states", "active", 1, "`name`='KODI'");
 				return "true";
 			}
-	
+
 			else {
 				SQLITE::update("states", "active", 0, "`name`='KODI'");
 				return "false";
 			}
-
-		/*
-		$res = SQLITE::fetch("states", ["active"], "name = 'KODI'");
-		$res = $res[0]["active"];
-		
-		if ($res > 0) return true;
-		else return false;
-		*/
 	}
-	
+
 	public static function on() {
-		LAN::SSH("KODI", "kodi > /dev/null &");
+		LAN::SSH("KODI", "kodi-standalone > /dev/null &");
 		SQLITE::update("states", "active", 1, "`name`='KODI'");
 		HomeBrain::wakecheck();
 		return "true";
     }
-	
+
 	public static function off() {
 		TV::off();
 		LAN::SSH("KODI", "kodi-send --action='Quit' > /dev/null &");
 		SQLITE::update("states", "active", 0, "`name`='KODI'");
 		return "true";
     }
-    
+
 }
 
 ?>

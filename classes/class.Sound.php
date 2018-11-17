2908577c22590281855f07534d324d1b5be5157a
Marijo <marijo@bubulescu.org>
2018-11-17 09:08:37 +0100

    code cleanup

--- classes/class.Sound.php
+++ classes/class.Sound.php
@@ -13,18 +13,18 @@ class Sound {
     public static function isOn() {
 	if ( Sound::isLoud() ) {
 		debug_log(__METHOD__.":".__LINE__, "Sound is loud..");
-			SQLITE::update("states", "active", 1, "`name`='Sound'");
+			SQLITE::update("states", "active", 1, "name='Sound'");
 			return "true";
 	}
 
         else {
-               SQLITE::update("states", "active", 0, "`name`='Sound'");
+               SQLITE::update("states", "active", 0, "name='Sound'");
                return "false";
         }
     }
 
     public static function isLoud() {
-	$res = SQLITE::query("SELECT AVG(sound) FROM datalog WHERE DATETIME(timestamp) >= DATETIME('now', '-10 minutes')");
+	$res = SQLITE::query("SELECT AVG(sound) FROM datalog WHERE DATETIME(timestamp) >= DATETIME('now', '-5 minutes')");
 	return $res[0]["AVG(sound)"] > Configs::get("SOUND", "MAX");
     }
 }

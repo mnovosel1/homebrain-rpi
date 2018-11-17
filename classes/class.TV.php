6962e24c111f38dd1e0f424d6e2cdc40f1da403f
Marijo <marijo@bubulescu.org>
2018-11-17 09:07:25 +0100

    code cleanup

--- classes/class.TV.php
+++ classes/class.TV.php
@@ -18,18 +18,17 @@ class TV {
     public static function isOn() {
         if ( LAN::SSH("KODI", "echo 'pow 0' | /usr/bin/cec-client -s | grep 'power status:' | sed 's/power status: //'") == "on" ) {
 //	  if ( LAN::SSH("KODI", "/usr/bin/cec ison") == "on") {
-            SQLITE::update("states", "active", 1, "`name`='TV'");
+            SQLITE::update("states", "active", 1, "name='TV'");
             return "true";
         }
 
-        SQLITE::update("states", "active", 0, "`name`='TV'");
+        SQLITE::update("states", "active", 0, "name='TV'");
         return "false";
     }
 
     public static function on() {
         if (TV::isOn() == "false") {
             TV::power();
-            HomeBrain::wakecheck();
         }
         // LAN::SSH("KODI", "echo 'on 0' | /usr/bin/cec-client -s >> /dev/null &");
     }
@@ -37,7 +36,6 @@ class TV {
     public static function off() {
         if (TV::isOn() == "true") {
             TV::power();
-            HomeBrain::wakecheck();
         }
         // LAN::SSH("echo 'standby 0' | /usr/bin/cec-client -s >> /dev/null &");
     }

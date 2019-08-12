fd2cf1599e077ac00911f938656cbb334fe35b9a
Marijo <marijo@bubulescu.org>
2019-08-12 07:42:41 +0000

    mitemp update

---
+++ classes/helpers/mitemp/getmitemps.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+import logging
+import sys
+from datetime import datetime
+
+from btlewrap import available_backends, BluepyBackend, GatttoolBackend, PygattBackend
+from mitemp_bt.mitemp_bt_poller import MiTempBtPoller, \
+    MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
+
+class DevNull:
+    def write(self, msg):
+        pass
+
+sys.stderr = DevNull()
+
+def main():
+
+    i = 1
+    while i < 2:
+        try:
+            t = datetime.now()
+
+            poller = MiTempBtPoller("58:2d:34:32:62:fc", BluepyBackend)
+            living = "{}|{}|{}".format(poller.parameter_value(MI_BATTERY), poller.parameter_value(MI_TEMPERATURE), poller.parameter_value(MI_HUMIDITY))
+
+            poller = MiTempBtPoller("58:2d:34:32:66:fc", BluepyBackend)
+            bath = "{}|{}|{}".format(poller.parameter_value(MI_BATTERY), poller.parameter_value(MI_TEMPERATURE), poller.parameter_value(MI_HUMIDITY))
+        except:
+            i += 1
+            print("Polling failed.")
+            pass
+        else:
+            f = open("../../var/mitemps.log", "w")
+            f.write("{}|livingroom|{}|bathroom|{}".format(t.strftime('%d/%m|%H:%M'), living, bath))
+            f.close()
+            print("Polling done, temps.log written")
+            break
+
+if __name__ == '__main__':
+    main()

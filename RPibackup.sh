790cc4d6920d2571bac97855c684a4a78c7e88fe
Marijo <marijo@bubulescu.org>
2019-08-24 12:39:53 +0200

    RPibackup updated to backup on hbrain and hbserver

--- RPibackup.sh
+++ RPibackup.sh
@@ -2,29 +2,29 @@
 
 date=`date +"%d-%m-%Y"`
 
-mount /srv/storage/
+#mount /srv/storage/
 
 /etc/init.d/homebrain stop
 #/etc/init.d/openvpn stop
 /etc/init.d/apache2 stop
 /etc/init.d/cron stop
 
-echo "Backing up to /srv/storage/2_backups/RPi_backup/ ..."
+echo "Backing up to /srv/PiStorage/backups/ && hbserver ..."
 
-echo "Backup /home/hbrain/.node-red to node-red_$date.tgz...."
-tar -zcf /srv/storage/2_backups/RPi_backup/node-red_$date.tgz /home/hbrain/.node-red/
+echo "Backup /home/hbrain/.node-red to hbrain_node-red_$date.tgz...."
+tar -zcf /srv/PiStorage/backups/hbrain_node-red_$date.tgz /home/hbrain/.node-red/
 
-echo "Backup /srv/HomeBrain to RPi_etc_$date.tgz...."
-tar -zcf /srv/storage/2_backups/RPi_backup/RPi_DIR-etc_$date.tgz /etc/
+echo "Backup /etc to hbrain_etc_$date.tgz...."
+tar -zcf /srv/PiStorage/backups/hbrain_DIR-etc_$date.tgz /etc/
 
-echo "Backup /srv/HomeBrain to RPi_hbraindir_$date.tgz...."
-tar -zcf /srv/storage/2_backups/RPi_backup/RPi_DIR-hbraindir_$date.tgz /srv/HomeBrain/
+echo "Backup /srv/HomeBrain to hbrain_hbraindir_$date.tgz...."
+tar -zcf /srv/PiStorage/backups/hbrain_DIR-hbraindir_$date.tgz /srv/HomeBrain/
 
-echo "Backup /boot to RPi_boot_$date.img...."
-dd if=/dev/mmcblk0p1 | ssh hbrain@10.10.10.100 dd of=/srv/storage/2_backups/RPi_backup/RPi_mmcblk0p1-boot_$date.img bs=1024k status=progress
+echo "Backup /boot to hbrain_boot_$date.img...."
+dd if=/dev/mmcblk0p1 | ssh hbrain@10.10.10.100 dd of=/srv/storage/2_backups/RPi_backup/hbrain_mmcblk0p1-boot_$date.img bs=1024k status=progress
 
-echo "Backup / to RPi_root_$date.img...."
-dd if=/dev/sda2 | ssh hbrain@10.10.10.100 dd of=/srv/storage/2_backups/RPi_backup/RPi_sda2-root_$date.img bs=1024k status=progress
+echo "Backup / to hbrain_root_$date.img...."
+dd if=/dev/sda2 | ssh hbrain@10.10.10.100 dd of=/srv/storage/2_backups/RPi_backup/hbrain_sda2-root_$date.img bs=1024k status=progress
 
 /etc/init.d/cron start
 /etc/init.d/apache2 start

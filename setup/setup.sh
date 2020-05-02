3398998b0f05e878372057ea9441b4cd185a2a2f
Marijo <marijo@bubulescu.org>
2020-05-02 12:13:29 +0100

    Setup steps

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo ln -s /srv/HomeBrain/.aliases /etc/profile.d/00-homebrain_aliases.sh
+sudo ln -s /srv/HomeBrain/setup/.aliases /etc/profile.d/00-homebrain_aliases.sh
+
+sudo mv /var/www/html /var/www/html_old
+sudo ln -s /srv/HomeBrain/www /var/www/html
+
+sudo cp /srv/HomeBrain/setup/homebrain.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable homebrain.service
+sudo systemctl start homebrain.service
